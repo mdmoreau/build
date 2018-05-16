0766366428ecdcf89c02a10020ced3c1bcd124e8
mdmoreau <mdmoreau@gmail.com>
2018-05-16 15:22:22 -0400

    add rollup npm script

---
+++ config/rollup.config.js
@@ -0,0 +1,30 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import uglify from 'rollup-plugin-uglify';
+
+const globby = require('globby');
+
+const configs = globby.sync('src/js/*.js').map(inputFile => ({
+  input: inputFile,
+  output: {
+    file: inputFile.replace('src', 'dist').replace('.js', '.min.js'),
+    format: 'iife',
+  },
+  plugins: [
+    resolve(),
+    commonjs(),
+    babel({
+      exclude: 'node_modules/**',
+      presets: [
+        ['@babel/preset-env', {
+          modules: false,
+          useBuiltIns: 'usage',
+        }],
+      ],
+    }),
+    uglify(),
+  ],
+}));
+
+module.exports = configs;
