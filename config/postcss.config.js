733e8b242dcf378219d4e51ae8d331061a0683ab
mdmoreau <mdmoreau@gmail.com>
2018-05-21 00:21:16 -0400

    replace csso with clean

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -2,7 +2,7 @@ const atImport = require('postcss-import');
 const mixins = require('postcss-mixins');
 const presetEnv = require('postcss-preset-env');
 const inlineSvg = require('postcss-inline-svg');
-const csso = require('postcss-csso');
+const clean = require('postcss-clean');
 
 module.exports = {
   plugins: [
@@ -14,8 +14,12 @@ module.exports = {
     inlineSvg({
       path: 'img',
     }),
-    csso({
-      comments: false,
+    clean({
+      level: {
+        1: {
+          specialComments: 0,
+        },
+      },
     }),
   ],
 };
