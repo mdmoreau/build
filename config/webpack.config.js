8e58a35748e1a6159cfa33f492eb8cc8b84b5035
mdmoreau <mdmoreau@gmail.com>
2018-12-12 22:04:26 -0500

    replace rollup with webpack

---
+++ config/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: './src/js/main.js',
+  },
+  output: {
+    filename: '[name].min.js',
+    path: path.resolve(__dirname, '../dist/js'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: /src\/js/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { useBuiltIns: 'usage' }],
+            ],
+          },
+        },
+      },
+    ],
+  },
+  mode: 'production',
+};
