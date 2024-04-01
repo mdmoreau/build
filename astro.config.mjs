e9b39bcc731816e647892828c2e65cb0cd5a4e70
mdmoreau <mdmoreau@gmail.com>
2024-03-31 23:32:49 -0400

    switch to astro

---
+++ astro.config.mjs
@@ -0,0 +1,26 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  build: {
+    format: 'file',
+  },
+  devToolbar: {
+    enabled: false,
+  },
+  vite: {
+    build: {
+      assetsInlineLimit: 0,
+      cssCodeSplit: false,
+      rollupOptions: {
+        output: {
+          entryFileNames: 'assets/script.js',
+          assetFileNames: 'assets/[name][extname]',
+        }
+      }
+    },
+    css: {
+      transformer: 'lightningcss',
+    }
+  }
+});
