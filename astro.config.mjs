5709f8fe7cc08fff4a22c0b3d6f13537a7da024b
mdmoreau <mdmoreau@gmail.com>
2024-12-31 19:06:29 -0500

    clean up dist

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,6 @@
+import fs from 'node:fs';
+import path from 'node:path';
+
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
@@ -8,6 +11,28 @@ export default defineConfig({
   devToolbar: {
     enabled: false,
   },
+  integrations: [
+    {
+      name: 'bundle',
+      hooks: {
+        'astro:build:done': (options) => {
+          const { pathname } = options.dir;
+          const html = fs.globSync(path.join(pathname, '*.html'));
+          const scripts = fs.globSync(path.join(pathname, 'assets/script[0-9]*.js'));
+          const chunksDir = path.join(pathname, 'chunks');
+          for (const file of html) {
+            let data = fs.readFileSync(file, 'utf8');
+            data = data.replace(/<script type="module" src=".*\/assets\/script\d+\.js"><\/script>/, '');
+            fs.writeFileSync(file, data);
+          }
+          for (const script of scripts) {
+            fs.rmSync(script);
+          }
+          fs.rmSync(chunksDir, { recursive: true });
+        }
+      }
+    }
+  ],
   vite: {
     build: {
       assetsInlineLimit: 0,
