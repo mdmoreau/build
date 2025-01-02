a952be51f7e29c9c11698e0880614db11cc5bdeb
mdmoreau <mdmoreau@gmail.com>
2025-01-02 14:24:51 -0500

    refactor

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,8 +15,8 @@ export default defineConfig({
     {
       name: 'bundle',
       hooks: {
-        'astro:build:done': (options) => {
-          const { pathname } = options.dir;
+        'astro:build:done': ({ dir }) => {
+          const { pathname } = dir;
           const html = fs.globSync(path.join(pathname, '*.html'));
           const scripts = fs.globSync(path.join(pathname, 'assets/script[0-9]*.js'));
           const chunksDir = path.join(pathname, 'chunks');
