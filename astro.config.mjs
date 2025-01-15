c82c0dfbfddb8f16c955006bb0fa47f4d3120f15
mdmoreau <mdmoreau@gmail.com>
2025-01-15 02:43:45 -0500

    downgrade astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from 'astro/config';
-
-import fs from 'node:fs';
-import path from 'node:path';
 import nesting from 'postcss-nesting';
 
 // https://astro.build/config
@@ -12,28 +9,6 @@ export default defineConfig({
   devToolbar: {
     enabled: false,
   },
-  integrations: [
-    {
-      name: 'bundle',
-      hooks: {
-        'astro:build:done': ({ dir }) => {
-          const { pathname } = dir;
-          const html = fs.globSync(path.join(pathname, '*.html'));
-          const scripts = fs.globSync(path.join(pathname, 'assets/script[0-9]*.js'));
-          const chunksDir = path.join(pathname, 'chunks');
-          for (const file of html) {
-            let data = fs.readFileSync(file, 'utf8');
-            data = data.replace(/<script type="module" src=".*\/assets\/script\d+\.js"><\/script>/, '');
-            fs.writeFileSync(file, data);
-          }
-          for (const script of scripts) {
-            fs.rmSync(script);
-          }
-          fs.rmSync(chunksDir, { recursive: true });
-        }
-      }
-    }
-  ],
   vite: {
     build: {
       assetsInlineLimit: 0,
@@ -42,7 +17,6 @@ export default defineConfig({
         output: {
           assetFileNames: 'assets/[name][extname]',
           entryFileNames: 'assets/script.js',
-          manualChunks: () => 'bundle',
         }
       }
     },
